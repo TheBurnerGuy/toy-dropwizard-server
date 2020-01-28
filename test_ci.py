bba35629cffdce96406724f53731ae086516a452
TheBurnerGuy <henryweilo@hotmail.com>
2020-01-27 18:18:43 -0700

    First draft of new CI in ci.yml, added 2 tests to test_ci

--- test_ci.py
+++ test_ci.py
@@ -8,3 +8,20 @@ def test_health_check():
 # 1. Add two more tests here of your choice. I will explain the API.
 #    Make sure to verify the necessary info, e.g., status code, response data.
 # 2. Add "pytest" as integration testing script as part of Github Actions CI workflow (you've done this before!)
+# Test if id goes up
+def test_id_check():
+    r = requests.get('http://localhost:8085/hello-world')
+    request_json = r.json()
+    old_id = request_json['id']
+
+    r = requests.get('http://localhost:8085/hello-world')
+    request_json = r.json()
+    new_id = request_json['id']
+
+    assert(new_id == old_id + 1)
+
+# Test if name is returned
+def test_name_check():
+    r = requests.get('http://localhost:8085/hello-world?name=Henry')
+    request_json = r.json()
+    assert(request_json['content'] == 'Hello, Henry!')
